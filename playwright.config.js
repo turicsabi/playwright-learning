5046a67345f214e747e9103d2653c1eb54154587
Turi <csaba.kereszturi@globallogic.com>
2024-03-15 10:33:16 +0200

    Added headed mode

--- playwright.config.js
+++ playwright.config.js
@@ -44,7 +44,7 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'], 
-      headless: true,
+      headless: false,
       ignoreHTTPSErrors: true,
       viewport: {width: width, height: height},
       }
